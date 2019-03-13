358c9c688a2ec18d8e612993cc2c0b0eaf7374a5
Devender Kumar <deku@comp-tdjakba.imm.dtu.dk>
2019-03-13 15:18:14 +0100

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <movisens_flutter/MovisensFlutterPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [MovisensFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"MovisensFlutterPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
